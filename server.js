629015e7f2e75516c714e2219dfcdb8e5408bc05
Rohan Jain <rohan19095@iiitd.ac.in>
2020-08-11 00:15:51 +0530

    Build

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-
+const path = require("path");
 const items = require("./routes/api/items");
 
 const app = express();
@@ -15,6 +15,15 @@ mongoose
 	.catch((err) => console.log(err));
 
 app.use("/api/items", items);
+
+app.get("*", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+// Serve static assets If in production
+if (process.env.NODE_ENV === "production") {
+	//Set static folder
+	app.use(express.static("client/build"));
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
